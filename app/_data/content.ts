e2799d965f8af4ab863c2548b5baef1c905f2498
Ties <ties@vanoldenborgh.com>
2026-02-24 14:41:06 +0400

    Initial commit

--- app/_data/content.ts
+++ app/_data/content.ts
@@ -1,10 +1,10 @@
 export const content = {
   name: "Nathalya",
   title: "Model & Creator",
-  tagline: "Welcome.. find my spicy links here. Will you send me a message babe?",
+  tagline: "your army girl next door 🤭 send me a message 👀",
   ctas: [
-    { label: "Onlyfans", href: "https://onlyfans.com/dutcharmygirl", brand: "onlyfans" },
-    { label: "F2F", href: "https://f2f.com/nathalya", brand: "f2f" },
+    { label: "Onlyfans", href: "https://onlyfans.com/dutcharmygirl/c1", brand: "onlyfans" },
+    { label: "F2F", href: "https://f2f.com/s/lJ7YwM", brand: "f2f" },
   ],
   gallery: Array.from({ length: 19 }).map((_, i) =>
     `/gallery/${String(i + 1).padStart(3, "0")}.jpg`
